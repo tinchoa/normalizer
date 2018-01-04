8bf90941a851acdeb25acf0e700efefcc8778af6
tinchoa <martinandreoni@gmail.com>
2018-01-04 13:39:57 +0100

    function updated

--- new.py
+++ new.py
@@ -18,10 +18,13 @@ def getValues(janela):
 	sigmin=[]    
 	for i in range(len(janela[0])):
 		column=janela[:,i].astype(np.float64)
-		vmax.append(max(column))
-		vmin.append(min(column))
+		localMax=max(column)
+		vmax.append(localMax)
+		localMin=min(column)
+		vmin.append(localMin)
 		umean.append(np.mean(column))
 		sigmin.append(np.std(column)) 
+		
 	return vmax,vmin,umean,sigmin
 
 
@@ -160,20 +163,20 @@ def relativeFreq(histogram, numberSamples):
 	function to create the relative frequenci of each bin
 	'''
 	relative={}
-	aux=[]
+	aux={}
 	for feature in range(len(histogram)):
-		relative[feature]=[]
-		#for feature in histogram[windows]:
-		for bins in histogram[feature]:
+		aux[feature] = {k: [] for k in range(len(histogram[feature]))} #initialize dict of list
+
+		for bins in range(len(histogram[feature])):
 		#	print bins
 			#aux.append(bins/float(numberSamples)) #isso aqui esta dando muito baixo (e acava caindo tudo perto de zero)
-			aux.append(bins/float(50)) #numeros de amostras no bin
-		relative[feature]=aux
-		aux=[]
+			aux[feature][bins]=(histogram[feature][bins]/float(50)) #numeros de amostras no bin
+		#relative[feature][bins]=aux[feature][bins]
+		#aux=[]
 		#		histogram[feature][bins]
 				#p=[x.astype(np.float64) for x in jan[:,i] if x.astype(np.float64) >= bins[i][j][0] and x.astype(np.float64) < bins[i][j][1]]
 
-	return relative
+	return aux
 
 
 def calculateZ(relative):
@@ -181,20 +184,30 @@ def calculateZ(relative):
 	'''
 	global N
 	Z={}
-	aux=[]
-	for windows in range(N):
-		Z[windows]=[]
+	for feature in range(N):
+		Z[feature] = {k: [] for k in range(len(relative[feature]))} #initialize dict of list
 		#for feature in relative[windows]:
-		for bins in relative[windows]:
-			if (bins == 0.0):
-				aux.append(st.norm.cdf(0))
+		for bins in relative[feature]:
+			if (relative[feature][bins] == 0.0):
+				Z[feature][bins]=(st.norm.cdf(0))
 			else:
-				p=filter(lambda x : x < bins, relative[windows])
-				aux.append(st.norm.cdf(sum(p)))
-		Z[windows]=aux
-		aux=[]
-
+				p=filter(lambda x : x < relative[feature][bins], relative[feature].values())
+				Z[feature][bins]=st.norm.cdf(sum(p))
+		
 	return Z
+
+def backZ2values(rawValues,Zvalues):
+	'''
+	this function will pass the Zvalues to the rawvalues 
+	'''
+	newValues={}
+	for feature in Zvalues:
+		newValues[feature] = {k: [] for k in range(len(relative[feature]))} #initialize dict of list
+		for bins in Zvalues[feature]:
+			newValues[feature][bins]=map(lambda x: Zvalues[feature][bins],rawValues[feature][bins])
+
+	return newValues
+
 
 
 global windowSize
@@ -248,9 +261,7 @@ rawValues={}
 
 Zvalues={}
 
-maxGlobal=[]
-minGlobal=[]
-
+newValues={}
 
 for i in range(0,len(batch), windowSize): #
 		
@@ -259,41 +270,34 @@ for i in range(0,len(batch), windowSize): #
 		localMax,localMin,localMean,localStd = getValues(jan)
 		
 		if windowsNumber==0:
+			maxGlobal,minGlobal=localMax,localMin
 			numberSamples= (N*windowSize)
 			binsTotal=createBins(localMax,localMin)
 			histogram,rawValues=(createHistogram(jan,binsTotal))
 		else:
+			binsTotal=updateBins(localMax,localMin,binsTotal)	
 			numberSamples=(N*windowSize*windowsNumber)
-			binsTotal=updateBins(localMax,localMin,binsTotal)
 			histogram,rawValues=(updateHistogram(jan,binsTotal,histogram,rawValues))
 			
-		#relative=(relativeFreq(histogram,numberSamples))
+			relative=(relativeFreq(histogram,numberSamples))
 
-		#Zvalues[windowsNumber]=(calculateZ(relative))
+			Zvalues=(calculateZ(relative))
 
+			newValues=backZ2values(rawValues,Zvalues)
 
-# 		for x in jan[:,12]:
-# 			for bind in range(len(binsTotal[12])): #num of bins
-# 				if (x.astype(np.float64) >= binsTotal[feature][bind][0] and x.astype(np.float64) < binsTotal[feature][bind][1]):
-# 					print  binsTotal[feature][bind][0],binsTotal[feature][bind][1]
-# 					print x
-	 	#print histogram[23]
-	 	print binsTotal[23]
-	 	print histogram[23]
-	 	#print rawValues[19]
 	 	windowsNumber+=1 #incrementing this number
 
 
 
-t=0
-for i in range(len(histogram)):
-	for j in histogram[i]:
-		if (histogram[i].values().count(0) > 7):
-			f=1000
-		else:
-			f=sum(histogram[i].values())
-	t+=f
-	if f != 1000:
-		print i
-		print f
+# t=0
+# for i in range(len(histogram)):
+# 	for j in histogram[i]:
+# 		if (histogram[i].values().count(0) > 7):
+# 			f=1000
+# 		else:
+# 			f=sum(histogram[i].values())
+# 	t+=f
+# 	if f != 1000:
+# 		print i
+# 		print f
 
