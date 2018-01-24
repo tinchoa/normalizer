ceb15770420fe0f51606c510097f86627966f560
tinchoa <martinandreoni@gmail.com>
2018-01-24 19:46:03 +0100

    /tmp

--- normalizerOffline.py
+++ normalizerOffline.py
@@ -7,7 +7,7 @@ from sklearn import preprocessing
 class normalizer:
 
 	def run(self):
-		w = open("/tmp/classes-17-norm-offline.out",'w')
+		w = open("classes-17-norm-offline.out",'w')
 		f = open("classes-17.out",'r')
 		linha = f.readline()
 		vetor = [float(i) for i in linha.split(",")[5:-1]]
