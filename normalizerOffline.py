62cb3f62467003b4cc3e56c10dc74ecb47fca6f1
tinchoa <martinandreoni@gmail.com>
2018-01-24 19:48:08 +0100

    tabs

--- normalizerOffline.py
+++ normalizerOffline.py
@@ -15,30 +15,30 @@ class normalizer:
 		vetorMax = vetor
 		vetorMin = [float(i) for i in linha.split(",")[5:-1]]
 		while linha != "":
-		    vetor = [float(i) for i in linha.split(",")[5:-1]]
-		    for i in range(len(vetor)):
-		        if vetor[i] > vetorMax[i]:
-		            vetorMax[i] = vetor[i]
-		        if vetor[i] < vetorMin[i]:
-		            vetorMin[i] = vetor[i]
-		    linha = f.readline()
+			vetor = [float(i) for i in linha.split(",")[5:-1]]
+			for i in range(len(vetor)):
+				if vetor[i] > vetorMax[i]:
+					vetorMax[i] = vetor[i]
+				if vetor[i] < vetorMin[i]:
+					vetorMin[i] = vetor[i]
+			linha = f.readline()
 
 		f = open("classes-17.out",'r')
 		linha = f.readline()
 		salida=[]
 		while linha != "":
-		    vetor = [float(i) for i in linha.split(",")[5:-1]]
-		    for i in range(len(vetor)):
-		        try:
-		            vetor[i] = (vetor[i]-vetorMin[i])/(vetorMax[i]-vetorMin[i])
-		        except ZeroDivisionError:
-		            if (vetorMin[i]+vetorMax[i]) > 0: vetor[i] = (vetor[i])/(vetorMax[i]+vetorMin[i])
-		            else: vetor[i] = 0.5
-		    linha = f.readline()
+			vetor = [float(i) for i in linha.split(",")[5:-1]]
+			for i in range(len(vetor)):
+				try:
+					vetor[i] = (vetor[i]-vetorMin[i])/(vetorMax[i]-vetorMin[i])
+				except ZeroDivisionError:
+					if (vetorMin[i]+vetorMax[i]) > 0: vetor[i] = (vetor[i])/(vetorMax[i]+vetorMin[i])
+					else: vetor[i] = 0.5
+			linha = f.readline()
 			#armazanar em arquivo
-		   	vetorStr = [str(i) for i in vetor]
-		    w.write(",".join(vetorStr)+"\n")
-		    salida.append(vetor)
+			vetorStr = [str(i) for i in vetor]
+			w.write(",".join(vetorStr)+"\n")
+			salida.append(vetor)
 		
 		for i in range(len(salida)):
 			if i==0:
